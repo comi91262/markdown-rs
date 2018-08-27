d80f34633d136bad4977689fb7dc9d190565e13f
taba <hgKaguyaToupako4er@gmail.com>
2018-08-27 19:47:09 +0900

    Add thematic_break example13

---
+++ src/block_parser/mod.rs
@@ -0,0 +1,187 @@
+use pest::Parser;
+
+#[cfg(debug_assertions)]
+const _GRAMMAR: &'static str = include_str!("block.pest");
+
+#[derive(Parser)]
+#[grammar = "block_parser/block.pest"]
+struct BlockParser;
+
+#[derive(Debug)]
+pub enum BlockType {
+    Document,
+    ThematicBreaks,
+    //    AtxHeadings,
+    //    BlockQuote,
+    //    Paragraph,
+    //    List,
+    //    ListItem,
+}
+
+#[derive(Debug)]
+pub struct Block {
+    pub block_type: BlockType,
+    pub children: Vec<Block>,
+    is_closed: bool,
+    raw_text: String,
+}
+
+impl Block {
+    fn add(&mut self, block_type: BlockType, text: String) {
+        let child = Block {
+            is_closed: false,
+            block_type: block_type,
+            raw_text: text,
+            children: vec![],
+        };
+
+        self.children.push(child);
+    }
+
+    fn get(&self) -> &Block {
+        &self
+    }
+}
+
+pub fn parse(line: &str) -> Block {
+    let tokens = BlockParser::parse(Rule::document, line).unwrap_or_else(|e| panic!("{}", e));
+
+    let mut root_block = Block {
+        is_closed: false,
+        block_type: BlockType::Document,
+        raw_text: "".to_string(),
+        children: vec![],
+    };
+
+    for token in tokens {
+        match token.as_rule() {
+            Rule::thematic_break => {
+                root_block.add(BlockType::ThematicBreaks, "".to_string());
+            }
+            _ => (),
+        }
+    }
+    println!("{:?}", root_block);
+    root_block
+}
+
+// > Lorem ipsum dolor
+// sit amet.
+// > - Qui *quodsi iracundia*
+// > - aliquando id
+//-> document
+//  -> block_quote
+//       paragraph
+//         "Lorem ipsum dolor\nsit amet."
+//    -> list (type=bullet tight=true bullet_char=-)
+//         list_item
+//           paragraph
+//             "Qui *quodsi iracundia*"
+//      -> list_item
+//        -> paragraph
+//             "aliquando id"
+
+#[test]
+fn test_t() {
+    // Example 13
+    parses_to! {
+        parser: BlockParser,
+        input: "***\n---\n___\n",
+        rule: Rule::document,
+        tokens: [
+          thematic_break(0, 4, [
+    //      astar(0, 1),
+    //      astar(1, 2),
+    //      astar(2, 3),
+          line_ending(3, 4),
+          ]),
+          thematic_break(4, 8, [
+          hyphen(4, 5),
+          hyphen(5, 6),
+          hyphen(6, 7),
+          line_ending(7, 8),
+          ]),
+          thematic_break(8, 12, [
+          under(8, 9),
+          under(9, 10),
+          under(10, 11),
+          line_ending(11, 12),
+          ])
+        ]
+    };
+}
+
+//fn test_s() {
+//    let mut root_block = Block {
+//        is_closed: false,
+//        block_type: BlockType::Document,
+//        raw_text: "".to_string(),
+//        children: vec![],
+//    };
+//
+//    for token in parse("***\n---\n___\n") {
+//        match token {
+//            thematic_break => root_block.add(BlockType::AtxHeadings, "".to_string()),
+//            _ => println!("{:?}", token),
+//
+//        }
+//
+//    }
+//
+//     println!("{:?}", root_block);
+//
+//}
+//
+
+//# foo
+//## foo
+//### foo
+//#### foo
+//##### foo
+//###### foo
+//####### foo
+//
+//<p>####### foo</p>
+//
+//<h1>foo</h1>
+//<h2>foo</h2>
+//<h3>foo</h3>
+//<h4>foo</h4>
+//<h5>foo</h5>
+//<h6>foo</h6>
+//    root_block.add(BlockType::BlockQuote, "".to_string());
+//
+//    let block2 = Block {
+//        is_closed: false,
+//        block_type: BlockType::Document,
+//        raw_text: "".to_string(),
+//        children: vec![Block {
+//            is_closed: false,
+//            block_type: BlockType::BlockQuote,
+//            raw_text: "".to_string(),
+//            children: vec![
+//                Block {
+//                    is_closed: false,
+//                    block_type: BlockType::Paragraph,
+//                    raw_text: "Lorem ipsum dolor\nsit amet.".to_string(),
+//                    children: vec![],
+//                },
+//                Block {
+//                    is_closed: false,
+//                    block_type: BlockType::List,
+//                    raw_text: "".to_string(),
+//                    children: vec![],
+//                },
+//            ],
+//        }],
+//    };
+//
+//    println!("{:?}", root_block);
+//
+//pest::parses_to! {
+//    parser: MarkdownParser,
+//    input: "# 1",
+//    rule: Rule::exp,
+//    tokens: [
+//    ]
+//};
---
+++ src/translator.rs
@@ -0,0 +1,38 @@
+use block_parser;
+use block_parser::Block;
+use block_parser::BlockType;
+
+fn print(tree: block_parser::Block) -> String {
+    match tree {
+        Block {
+            block_type: BlockType::Document,
+            children,
+            ..
+        } => {
+            let mut result_str = String::new();
+            for v in children {
+                result_str.push_str(&print(v))
+            }
+            result_str
+        }
+        Block {
+            block_type: BlockType::ThematicBreaks,
+            ..
+        } => {
+            let mut result_str = String::with_capacity(6);
+            result_str.push_str("<hr />");
+            result_str
+        }
+    }
+}
+
+pub fn exec(input_str: &str) -> String {
+    let tree = block_parser::parse(input_str);
+    print(tree)
+}
+
+#[test]
+fn test_exec() {
+    let tree = exec("***\n---\n___\n");
+    assert_eq!(tree, "<hr /><hr /><hr />");
+}
